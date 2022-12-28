1d49305be4ab719345588f6f52667592fcb95545
Rizwanation <m.rizwan07@gmail.com>
2022-12-28 19:42:52 +0500

    Initial basic maven project setup with successful test execution

---
+++ src/test/java/org/cbh/tests/BasicTest.java
@@ -0,0 +1,75 @@
+package org.cbh.tests;
+
+import com.google.common.util.concurrent.Uninterruptibles;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class BasicTest {
+
+    @Test
+    private void endToEndTest()
+    {
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "//Drivers//chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+
+        driver.get("https://www.amazon.in");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        WebElement hamburgerMenu = driver.findElement(By.xpath("//a[@id='nav-hamburger-menu']"));
+
+        wait.until(ExpectedConditions.visibilityOf(hamburgerMenu)).click();
+        WebElement category = driver.findElement(By.xpath("//div[text()='TV, Appliances, Electronics']/parent::a"));
+        wait.until(ExpectedConditions.visibilityOf(category)).click();
+
+        WebElement subCategory = driver.findElement(By.xpath("//a[text()='Televisions' and @class='hmenu-item']"));
+        wait.until(ExpectedConditions.visibilityOf(subCategory)).click();
+
+        WebElement brand = driver.findElement(By.xpath("//span[text()='Samsung' and @dir='auto']"));
+        wait.until(ExpectedConditions.visibilityOf(brand)).click();
+
+        WebElement drpDown = driver.findElement(By.xpath("//select[@name='s']"));
+        wait.until(ExpectedConditions.visibilityOf(drpDown));
+
+        Select dropDown = new Select(drpDown);
+        dropDown.selectByVisibleText("Price: High to Low");
+
+        List<WebElement> allProducts = driver.findElements(By.xpath("//div[@class='a-section a-spacing-base']//h2//a"));
+
+        wait.until(ExpectedConditions.visibilityOfAllElements(allProducts));
+        allProducts.get(1).click();
+
+
+        Iterator<String> windowHandles = driver.getWindowHandles().iterator();
+        while (windowHandles.hasNext())
+        {
+            driver.switchTo().window(windowHandles.next());
+        }
+
+
+
+        List<WebElement> aboutThisProdList = driver.findElements(By.xpath("//div[@id='feature-bullets']/ul/li"));
+
+        aboutThisProdList.forEach(x-> System.out.println(x.getText()));
+
+        Uninterruptibles.sleepUninterruptibly(Duration.ofSeconds(3));
+        driver.quit();
+
+    }
+}
